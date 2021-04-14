98dcb38ec524a28db837f11076e2cc7bdbcdf777
Bin <binlin35@yahoo.com>
2021-04-14 12:57:31 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -12,4 +12,87 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
 });
+
+
+let collection = document.querySelector('#toy-collection')
+let form = document.querySelector('.add-toy-form')
+let inputImg= document.querySelector('input[name=image]').value
+
+
+//Get toy list from back-end
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(theToy => {
+    theToy.forEach(function(toyList){ 
+      showCard(toyList)
+    })
+  })
+
+//Create new toy list
+
+form.addEventListener('submit', (evt) => {
+  let inputName = evt.target.querySelector('input[name=name]').value
+  let inputImag = evt.target.querySelector('input[name=image]').value
+  evt.preventDefault() 
+  console.log("Hello")
+  console.log(inputName)
+  console.log(inputImag)
+
+ fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name : inputName,
+      image : inputImag,
+      likes : 0
+    })
+  })
+  .then(res => res.json())
+  .then(newToy => {
+    showCard(newToy)
+  })
+})
+
+
+//Create toy list
+  function showCard(toyCard){
+
+    let card = document.createElement('div')
+    card.className = 'card'
+
+    let titles = document.createElement('h2')
+    titles.innerText = toyCard.name
+
+    let imgTag = document.createElement('img')
+    imgTag.className = 'toy-avatar'
+    imgTag.src = toyCard.image
+
+    let likeButton = document.createElement('button')
+    likeButton.innerText = 'like'
+    likeButton.id = toyCard.id
+    likeButton.className = 'like-btn'
+
+    let pTag = document.createElement('p')
+    pTag.innerText = toyCard.likes
+
+    card.append(titles, imgTag, pTag, likeButton)
+    collection.append(card)
+
+    likeButton.addEventListener('click', (evt) => {
+      fetch(`http://localhost:3000/toys/${toyCard.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({likes: toyCard.likes += 1})
+      })
+      .then(response => response.json())
+      .then(increaseLikes => {
+        pTag.innerText = increaseLikes.likes
+        toyCard.likes = increaseLikes.likes
+      })
+    })
+  }
